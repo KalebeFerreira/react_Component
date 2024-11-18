a9d57bc9c36d2f2ba68bb2b8ed3736640f0d397f
KalebeFerreira <kalebeferreira622@gmail.com>
2024-11-18 02:02:34 -0300

    Mensagem descritiva sobre o commit

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./MenuItem.css";
 
-function MenuItem({ onClick, label }) {
-  return <div onClick={onClick}>{label}</div>;
+function MenuItem({ onClick, label, icon }) {
+  return (
+    <div className="menu-item" onClick={onClick}>
+      {icon && <span className="icon">{icon}</span>} {/* Ícone */}
+      <span className="label">{label}</span> {/* Nome */}
+    </div>
+  );
 }
 
 export default MenuItem;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,61 +1,115 @@
 import React from "react";
 import MenuItem from "./MenuItem";
-import "./Sidebar.css";
+import { ReactComponent as Logo } from "../assets/icons/Logo.svg";
+import {
+  FaUser,
+  FaFileInvoiceDollar,
+  FaFolderOpen,
+  FaShieldAlt,
+  FaFile,
+  FaIdCard,
+  FaClipboardCheck,
+  FaCommentDots,
+  FaHome,
+  FaNetworkWired,
+  FaFileAlt,
+  FaCalendarDay,
+  FaTable,
+  FaChartLine,
+  FaMoneyBillAlt,
+} from "react-icons/fa"; // Importa os ícones necessários
+import "./Sidebar.css"; // Importa o CSS do Sidebar
 
 function Sidebar() {
-  const handleMenuClick = (item) => {};
+  const handleMenuClick = (item) => {
+    console.log(`Menu item clicked: ${item}`);
+  };
 
   return (
     <div className="sidebar">
-      <div className="Empresa">VOBYS</div>
-      <MenuItem label="Pessoas" onClick={() => handleMenuClick("Pessoas")} />
+      <div className="logo-container">
+        <Logo className="logo" />
+      </div>
+      <MenuItem
+        label="Pessoas"
+        icon={<FaUser className="icon" />}
+        onClick={() => handleMenuClick("Pessoas")}
+      />
       <MenuItem
         label="Folha de Pagamento"
+        icon={<FaFileInvoiceDollar className="icon" />}
         onClick={() => handleMenuClick("Folha de Pagamento")}
       />
       <MenuItem
         label="Arquivos Institucionais"
+        icon={<FaFolderOpen className="icon" />}
         onClick={() => handleMenuClick("Arquivos Institucionais")}
       />
       <MenuItem
         label="Segurança de Acesso"
+        icon={<FaShieldAlt className="icon" />}
         onClick={() => handleMenuClick("Segurança de Acesso")}
       />
-      <MenuItem label="e-Social" onClick={() => handleMenuClick("e-Social")} />
+      <MenuItem
+        label="e-Social"
+        icon={<FaFile className="icon" />}
+        onClick={() => handleMenuClick("e-Social")}
+      />
       <MenuItem
         label="Folha Estagiário / Menor Aprendiz / Bolsista"
+        icon={<FaIdCard className="icon" />}
         onClick={() =>
           handleMenuClick("Folha Estagiário / Menor Aprendiz / Bolsista")
         }
       />
       <MenuItem
         label="Concursos"
+        icon={<FaClipboardCheck className="icon" />}
         onClick={() => handleMenuClick("Concursos")}
       />
       <MenuItem
         label="Comunicação Administrativa"
+        icon={<FaCommentDots className="icon" />}
         onClick={() => handleMenuClick("Comunicação Administrativa")}
       />
-      <MenuItem label="Inicial" onClick={() => handleMenuClick("Inicial")} />
+      <MenuItem
+        label="Inicial"
+        icon={<FaHome className="icon" />}
+        onClick={() => handleMenuClick("Inicial")}
+      />
+      <MenuItem
+        label="Comunicação Administrativa"
+        icon={<FaCommentDots className="icon" />}
+        onClick={() => handleMenuClick("Comunicação Administrativa")}
+      />
       <MenuItem
         label="Integração"
+        icon={<FaNetworkWired className="icon" />}
         onClick={() => handleMenuClick("Integração")}
       />
       <MenuItem
         label="Processos Administrativos"
+        icon={<FaFileAlt className="icon" />}
         onClick={() => handleMenuClick("Processos Administrativos")}
       />
-      <MenuItem label="Diárias" onClick={() => handleMenuClick("Diárias")} />
+      <MenuItem
+        label="Diárias"
+        icon={<FaCalendarDay className="icon" />}
+        onClick={() => handleMenuClick("Diárias")}
+      />
       <MenuItem
         label="Tabelas Básicas"
+        icon={<FaTable className="icon" />}
         onClick={() => handleMenuClick("Tabelas Básicas")}
       />
       <MenuItem
         label="Gerencial"
+        icon={<FaChartLine className="icon" />}
         onClick={() => handleMenuClick("Gerencial")}
       />
       <MenuItem
         label="EFD-Reinf"
+        icon={<FaMoneyBillAlt className="icon" />}
         onClick={() => handleMenuClick("EFD-Reinf")}
       />
     </div>
